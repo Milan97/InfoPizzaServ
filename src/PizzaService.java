17ad88750112a175caf2ed132eb4e902b989dede
Cedric <cedrictd.ctd@gmail.com>
2016-01-03 18:36:15 +0100

    Buttons zum leben erweckt.

--- src/PizzaService.java
+++ src/PizzaService.java
@@ -1,7 +1,12 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -12,19 +17,22 @@ import javafx.stage.Stage;
  */
 public class PizzaService extends Application {
 
+    // -- Data Stuff -- //
+    DataGatherer data = new DataGatherer();
+    ObservableList<String> einkaufsWag = FXCollections.observableArrayList();
+
     // -- Controls -- //
     // Compose Order
-    Button addBtn = new Button("Hinzufügen");
+    Button addBtn = new Button("Hinzufuegen");
     Button removeBtn = new Button("Entfernen");
     Button cancelBtn = new Button("Abbrechen");
     // Send Order
     Button sendOrdBtn = new Button("Bestellung Senden");
     // Store Order
-    //TODO Figure out how Combo Boxes work
-    //ComboBox<String> Pizzen = new ComboBox<>()
+    ComboBox<String> Pizzen = new ComboBox<String>();
+    ListView<String> einkaufsWagView = new ListView<String>(einkaufsWag);
 
-    // -- Data Stuff -- //
-    DataGatherer data = new DataGatherer();
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -33,9 +41,42 @@ public class PizzaService extends Application {
         primaryStage.setScene(new Scene(root));
         primaryStage.setTitle("Pizza Service by Moritz, Milan, Cedric");
 
+        // Elemente ordnen
         setUpUI(root);
 
+        // Combo Box befüllen
+        ObservableList<String> pizzaVals = FXCollections.observableArrayList();
+        pizzaVals.addAll(data.getPizzas());
+        Pizzen.setItems(pizzaVals);
+        Pizzen.setValue(pizzaVals.get(0));
+
+        // Eventmanagement einrichten
+        setUpEvents();
+
         primaryStage.show();
+    }
+
+    private void setUpEvents() {
+        addBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                einkaufsWag.add(Pizzen.getValue());
+            }
+        });
+
+        removeBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                einkaufsWag.remove(einkaufsWag.size() - 1);
+            }
+        });
+
+        cancelBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                einkaufsWag.clear();
+            }
+        });
     }
 
     public static void main(String args[])  {
@@ -44,12 +85,15 @@ public class PizzaService extends Application {
 
     public void setUpUI(StackPane root) {
         // Milan, hier kannst du dann die Ganzen Components aus Controls
-        // in root zu ner schönen UI verarbeiten, müsste dann im Fenster
+        // in root zu ner schönen UI anordnen, müsste dann im Fenster
         // auftauchen ^^
+        // Um den ganzen Back End Code kann ich mich kümmern, sorg du
+        // dann einfach dafür das des ganze Zeug mit Layouts und so
+        // gescheid aussieht.
 
         // Beispiel
         HBox box = new HBox();
-        box.getChildren().addAll(addBtn, removeBtn, cancelBtn);
+        box.getChildren().addAll(addBtn, removeBtn, cancelBtn, Pizzen, einkaufsWagView);
         root.getChildren().addAll(box);
     }
 }
